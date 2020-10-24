3ffdd858c7642b4cd85b85cfed08994fe409f18f
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-24 18:14:29 +0300

    Initial Commit

---
+++ GameInSwiftUI/GameInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameInSwiftUIApp.swift
+//  GameInSwiftUI
+//
+//  Created by Ramill Ibragimov on 24.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct GameInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
